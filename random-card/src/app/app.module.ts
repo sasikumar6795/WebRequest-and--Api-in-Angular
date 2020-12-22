7844d64a9cb9a5fe7b224feca7d200a81c693dcd
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2020-12-22 21:34:15 +0530

    adding httpclient module

--- random-card/src/app/app.module.ts
+++ random-card/src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//toaster
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
+//http
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+
+//font awesome
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome'
 
 @NgModule({
   declarations: [
@@ -10,7 +18,11 @@ import { CardComponent } from './card/card.component';
     CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(),
+    FontAwesomeModule // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
