113a435a55627e73af1051cf780f804266db7d33
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2020-12-23 19:53:37 +0530

    upto input decorator passed

--- random-card/src/app/card/card.component.ts
+++ random-card/src/app/card/card.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
+import {faEnvelope,faMapMarkedAlt, faPhone, faDatabase} from "@fortawesome/free-solid-svg-icons";
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+
+  @Input() user;
+
+  faEnvelope = faEnvelope;
+  faMapMarkedAlt =faMapMarkedAlt;
+  faPhone= faPhone;
+  faDatabase=faDatabase;
 
   constructor() { }
 
